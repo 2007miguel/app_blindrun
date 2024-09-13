c1425553b88f307bb48dec04cdb4e52215a610bf
2007miguel <148888334+2007miguel@users.noreply.github.com>
2024-09-13 07:39:53 -0500

    Initial commit

---
+++ build/intermediates/cxx/Debug/2d1l6f1y/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\juan\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\juan\\Documents\\Miguel\\lab-4\\prueba_opencv\\openCv\\.cxx\\Debug\\2d1l6f1y\\arm64-v8a" ^
+  opencv_jni_shared
